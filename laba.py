0ba27426963335bfd9d62e3b4656bdf376a8514e
serg <serg@yandex.ru>
2024-05-16 13:12:14 +0300

    рефакторинг в класс

--- laba.py
+++ laba.py
@@ -1,30 +1,42 @@
 import math
 import numpy as np
 
-
-def main(t, h, f, k):
-    ans = [f.copy()]
-    n = math.ceil(t / h)
-    for index in range(n):
-        f +=  np.dot(h * k,  f)  
-        ans.append(list(f.copy()))
-    return ans
-
-n = int(input("Введите количесво уравнений "))
-
-h = float(input("Введите длину шага "))
-t = float(input('Введите длительнсь процесса '))
-
-f = np.zeros(n)
-
-for i in range(n):
-    f[i] = float(input(f"Введите начальное значение для f_{i} "))
-
-k = np.zeros((n, n))
-for i in range(n):
-    for j in range(n):
-        k[i][j] = float(input(f"Введите значение для k_{i}_{j} "))
+class Solver:
+    n = None 
+    t = None 
+    f = None 
+    h = None 
+    k = None  
 
 
-ans = main(t, h, f, k)
-print(ans)
+    def __init__(self):
+        self.read()
+        ans = self.solve()
+        self.print(ans)
+
+    def read(self):
+        self.n = int(input("Введите количесво уравнений "))
+        self.h = float(input("Введите длину шага "))
+        self.t = float(input('Введите длительнсь процесса '))
+        self.f = np.zeros(self.n)
+        for i in range(self.n):
+            self.f[i] = float(input(f"Введите начальное значение для f_{i} "))
+        self.k = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                self.k[i][j] = float(input(f"Введите значение для k_{i}_{j} "))
+        
+
+    def solve(self):
+        n = math.ceil(self.t / self.h)
+        ans = np.zeros((n + 1, self.n))
+        ans[0] = self.f.copy()
+        for index in range(n):
+            ans[index + 1] = ans[index] + np.dot(self.h * self.k,  ans[index])
+        return ans.tolist()
+    
+    def print(self, ans):
+        print(ans)
+
+
+Solver()
